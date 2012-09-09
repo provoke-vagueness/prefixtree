9ff944bdc529155f0c8c7b6a6600c546534ea587
Aaron Iles <aaron.iles@gmail.com>
2012-09-09 20:27:29 +1000

    Add doctests of README
    Doctests are run as part of unittests.

--- tests/test_prefixtree.py
+++ tests/test_prefixtree.py
@@ -1,3 +1,5 @@
+import doctest
+
 try:
     # python 2.x
     import unittest2 as unittest
@@ -12,6 +14,9 @@ class TestPrefixTree(unittest.TestCase):
 
     def test_has_version(self):
         self.assertTrue(prefixtree.__version__)
+
+    def test_readme(self):
+        doctest.testfile('../README.rst')
 
     def test_has_prefixdict(self):
         self.assertTrue(hasattr(prefixtree, 'PrefixDict'))
